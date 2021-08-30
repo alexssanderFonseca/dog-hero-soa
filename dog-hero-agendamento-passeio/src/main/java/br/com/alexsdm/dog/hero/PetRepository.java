e4104cd942cd1f8a35436fa1a350dc9fe316178b
Alex <aleiftm@gmail.com>
2021-08-29 21:51:24 -0300

    Removendo classe desnecessaria

--- dog-hero-agendamento-passeio/src/main/java/br/com/alexsdm/dog/hero/PetRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.alexsdm.dog.hero;
-
-import br.com.alexsdm.dog.hero.domain.entity.Pet;
-import java.util.List;
-
-public interface PetRepository {
-
-    List<Pet> buscarPets(List<String> identificadores);
-}
